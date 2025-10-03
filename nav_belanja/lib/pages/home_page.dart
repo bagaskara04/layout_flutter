aa01275b2d27c40638d3ddb8d2e01955b3f949f4
bagaskara04 <sagabdespair01@gmail.com>
2025-10-03 23:02:50 +0700

    Praktikum 5 langkah 4,5, dan 6

--- nav_belanja/lib/pages/home_page.dart
+++ nav_belanja/lib/pages/home_page.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:nav_belanja/models/item.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
   @override
-  Widget build(BuildContext context){
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Shopping List',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Card(
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(child: Text(item.name)),
+                    Expanded(
+                      child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
